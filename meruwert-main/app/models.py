219f633f4cefbe2c12fc7521d252220ef240502d
meruwert <146689951+meruwert@users.noreply.github.com>
2023-11-22 07:57:20 +0600

    Add files via upload

---
+++ meruwert-main/app/models.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from app import app, db, login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(50), nullable=True)
+    last_name = db.Column(db.String(50), nullable=True)
+    phone_number = db.Column(db.String(50), nullable=True)
+    address = db.Column(db.String(100), nullable=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
+    password = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(255), nullable=True)
+
+
+
+with app.app_context():
+    db.create_all()
